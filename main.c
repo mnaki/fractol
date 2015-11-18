573a6b9add34a591e1a46eca786a33f1bd52404d
Nakiboudine Mohamed <nmohamed@student.42.fr>
2015-11-18 15:23:52 +0100

    mouse move

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: nmohamed <nmohamed@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/04/15 12:36:15 by nmohamed          #+#    #+#             */
-/*   Updated: 2015/11/18 14:59:43 by nmohamed         ###   ########.fr       */
+/*   Updated: 2015/11/18 15:21:53 by nmohamed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,12 +22,29 @@ void	new_env(ssize_t width, ssize_t height, char *title, t_env *e)
 	init_mouse_callbacks(e->mouse_callback);
 }
 
+int mouse_move(int x, int y, t_env *e);
+int mouse_move(int x, int y, t_env *e)
+{
+	static double	prev_x = 0;
+	static double	prev_y = 0;
+
+	e->wanted_im += (x - prev_x) / 10000;
+	e->wanted_re += (y - prev_y) / 10000;
+	e->move_x += (x - prev_x) / 1000;
+	e->move_y += (y - prev_y) / 1000;
+	prev_x = x;
+	prev_y = y;
+	ft_printf("x->\t%d\t\ty->\t%d", x, y);
+	return (1);
+}
+
 void	init_hooks(t_env *e)
 {
 	mlx_key_hook(e->win, &key_hook, e);
 	mlx_loop_hook(e->core, &loop_hook, e);
 	mlx_expose_hook(e->win, &expose_hook, e);
 	mlx_mouse_hook(e->win, &mouse_hook, e);
+	mlx_hook(e->win, 6, (1L<<6), mouse_move, e);
 }
 
 t_env	*global_singleton(void)
