b2babac8362608a6a6192a41419d7cd2847c5d0f
Nakiboudine Mohamed <nmohamed@student.42.fr>
2015-11-21 21:09:08 +0100

    truc

--- hook.c
+++ hook.c
@@ -6,7 +6,7 @@
 /*   By: nmohamed <nmohamed@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/04/16 09:38:00 by nmohamed          #+#    #+#             */
-/*   Updated: 2015/11/21 17:55:58 by nmohamed         ###   ########.fr       */
+/*   Updated: 2015/11/21 21:06:21 by nmohamed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,8 +49,8 @@ int mouse_move(int x, int y, t_env *e)
 	static double	prev_x = 0;
 	static double	prev_y = 0;
 
-	e->wanted_im += (x - prev_x) / 10000;
-	e->wanted_re += (y - prev_y) / 10000;
+	e->wanted_im += (x - prev_x) / 7500;
+	e->wanted_re += (y - prev_y) / 7500;
 	e->move_x += (x - prev_x) / (e->final_zoom * 1000);
 	e->move_y += (y - prev_y) / (e->final_zoom * 1000);
 	prev_x = x;
--- utils.c
+++ utils.c
@@ -6,13 +6,13 @@
 /*   By: nmohamed <nmohamed@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/18 14:07:46 by nmohamed          #+#    #+#             */
-/*   Updated: 2015/11/21 17:55:52 by nmohamed         ###   ########.fr       */
+/*   Updated: 2015/11/21 21:08:40 by nmohamed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-int		g_color = 0xff0000;
+int		g_color = 0xffff00;
 
 void	ft_swap(void *a, void *b)
 {
@@ -34,7 +34,7 @@ void	draw(void)
 	e = global_singleton();
 	while (1)
 	{
-		// clear_buffer();
+		clear_buffer();
 		e->c_re += ((e->wanted_re - e->c_re) / 4);
 		e->c_im += ((e->wanted_im - e->c_im) / 4);
 		e->max_iter += ((e->wanted_iter - e->max_iter) / 4);
@@ -57,7 +57,7 @@ void	plot(int *x, int *y, int *i)
 
 	e = global_singleton();
 	pthread_mutex_lock(&e->m);
-	put_pixel_to_img(*x, *y, (*i * g_color - -g_color), e);
+	put_pixel_to_img(*x, *y, *i * g_color & 0xffffff, e);
 	pthread_mutex_unlock(&e->m);
 }
 
@@ -79,6 +79,6 @@ void	clear_buffer(void)
 	pthread_mutex_lock(&e->m);
 	if (!e->run)
 		pthread_exit(0);
-	fill_img(((2 * 0x191919) * 0x009900), e);
+	// fill_img(((2 * 0x191919) * 0x009900), e);
 	pthread_mutex_unlock(&e->m);
 }
