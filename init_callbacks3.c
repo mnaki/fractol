f2857092a6dec7356938bf5848f00e466b57eebb
Nakiboudine Mohamed <nmohamed@student.42.fr>
2015-11-21 16:21:08 +0100

    keyrepeat

--- init_callbacks3.c
+++ init_callbacks3.c
@@ -6,7 +6,7 @@
 /*   By: nmohamed <nmohamed@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/18 13:51:26 by nmohamed          #+#    #+#             */
-/*   Updated: 2015/11/18 13:51:34 by nmohamed         ###   ########.fr       */
+/*   Updated: 2015/11/21 16:19:38 by nmohamed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,25 +15,25 @@
 int		ft_zoom_minus(void)
 {
 	ft_printf("%s\n", "unzoom");
-	global_singleton()->zoom *= 0.5;
+	global_singleton()->zoom *= 0.9;
 	return (1);
 }
 
 int		ft_zoom_plus(void)
 {
 	ft_printf("%s\n", "zoom");
-	global_singleton()->zoom *= 1.5;
+	global_singleton()->zoom *= 1.1;
 	return (1);
 }
 
 int		ft_increment_fractal(void)
 {
-	global_singleton()->wanted_iter += 10;
+	global_singleton()->wanted_iter += 1;
 	return (1);
 }
 
 int		ft_decrement_fractal(void)
 {
-	global_singleton()->wanted_iter -= 10;
+	global_singleton()->wanted_iter -= 1;
 	return (1);
 }
--- sets.c
+++ sets.c
@@ -6,7 +6,7 @@
 /*   By: nmohamed <nmohamed@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/18 13:51:48 by nmohamed          #+#    #+#             */
-/*   Updated: 2015/11/18 15:24:29 by nmohamed         ###   ########.fr       */
+/*   Updated: 2015/11/21 14:43:20 by nmohamed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,10 +26,10 @@ int		set1(void)
 
 int		set2(void)
 {
-	global_singleton()->set = 2;
-	global_singleton()->wanted_im = -0.42;
-	global_singleton()->wanted_re = -0.8;
-	global_singleton()->wanted_iter = 120;
+	global_singleton()->set = 1;
+	global_singleton()->wanted_im = 0.30;
+	global_singleton()->wanted_re = -0.75;
+	global_singleton()->wanted_iter = 30;
 	global_singleton()->zoom = 1;
 	global_singleton()->move_x = 0;
 	global_singleton()->move_y = 0;
